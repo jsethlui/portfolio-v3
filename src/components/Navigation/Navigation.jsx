2f059a0e465ec63e730b9deda26ca32e486655e2
Jeremy Louie <jeremysf74@yahoo.com>
2023-09-30 15:42:06 -0700

    Responsive styling * mobile and desktop responsive styling added to Intro and Navigation components * Installed react-responsive

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,19 +1,71 @@
 
-import { Tabs } from '@mantine/core'
+import { Tabs, Burger } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks';
 import { useNavigate, useParams } from 'react-router-dom'
+import { useMediaQuery } from 'react-responsive'
 import classes from '/src/components/Navigation/Navigation.module.css'
 
-function Navigation() {
+function MobileNavigationTabs() {
 
-    const iconColor = "var(--mantine-color-indigo-4)"
-    const navigate = useNavigate()
+    // @todo: add animations for when tabs are opened on mobile
+    // @todo: have tabs open on side
+
+    const navigate = useNavigate();
     const { tabValue } = useParams()
+    return (
+        <Tabs variant="unstyled"
+              classNames={classes}
+              value={ tabValue }
+              onChange={(value) => navigate(`#${value}`)}
+              orientation="vertical"
+              activateTabWithKeyboard={false}>
+            <Tabs.List>
+                <Tabs.Tab value="about">
+                    About
+                </Tabs.Tab>
 
+                <Tabs.Tab value="experience">
+                    Experience
+                </Tabs.Tab>
+
+                <Tabs.Tab value="projects">
+                    Projects
+                </Tabs.Tab>
+
+                <Tabs.Tab value="contact">
+                    Contact
+                </Tabs.Tab>
+
+                <Tabs.Tab value="resume">
+                    Resume
+                </Tabs.Tab>
+            </Tabs.List>
+        </Tabs>
+    )
+}
+
+function MobileNavigation() {
+    const [isOpen, { toggle }] = useDisclosure()
+    return (
+        <div>
+            <Burger className={classes.mobile}
+                    size="lg"
+                    opened={isOpen}
+                    onClick={toggle} />
+            { isOpen ? <MobileNavigationTabs /> : null }
+        </div>
+    )
+}
+
+function DesktopNavigation() {
+    const navigate = useNavigate();
+    const { tabValue } = useParams()
     return (
         <Tabs variant="unstyled"
             classNames={classes}
             value={ tabValue }
             onChange={(value) => navigate(`#${value}`)}
+            orientation="horizontal"
             activateTabWithKeyboard={false}>
             <Tabs.List>
                 <Tabs.Tab value="about">
@@ -37,6 +89,19 @@ function Navigation() {
                 </Tabs.Tab>
             </Tabs.List>
         </Tabs>
+    )
+}
+
+function Navigation() {
+    const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1224px)'})
+    const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+    const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+    return (
+        <div>
+            {isDesktopOrLaptop ? <DesktopNavigation /> : <MobileNavigation />}
+        </div>
     );
 }
 
